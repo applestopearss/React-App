92524bf1126fb9e17cec25740f5c1ff2f1cdfaea
TIMSCOMP\Tim <timratsko909@gmail.com>
2023-07-24 23:17:58 -0700

    Added mroe columns and sample data

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,9 +3,16 @@ import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper
 
 const CustomTable = () => {
   const [rows, setRows] = useState([
-    { id: 1, name: 'Item 1', status: 'Active' },
+    { id: 1, name: 'Item 1', pickup: 'Seattle at 7:30, 7/24/2023', delivery: 'Redmont at 7:30, 7/25/2023', status: 'Active' },
+    { id: 2, name: 'Item 2', pickup: 'Tacoma at 10:00, 7/27/2023', delivery: 'Olympia at 10:30, 7/30/2023', status: 'Inactive' },
+    { id: 3, name: 'Item 3', pickup: 'Seattle at 2:00, 7/15/2023', delivery: 'Spokane at 7:00, 7/20/2023', status: 'Inactive' },
+    { id: 4, name: 'Item 4', pickup: 'Auburn at 7:30, 7/24/2023', delivery: 'Everett at 3:00, 7/30/2023', status: 'Active' },
     // ... Add more initial rows if needed
   ]);
+
+  // Rows to add are Load number, pickup date, pickup time, pickup city, delivery city, delivery date, comments
+
+  
 
   const toggleStatus = (id) => {
     const newRows = rows.map(row => {
@@ -26,6 +33,8 @@ const CustomTable = () => {
         <TableHead>
           <TableRow>
             <TableCell>Name</TableCell>
+            <TableCell>Pickup</TableCell>
+            <TableCell>Delivery</TableCell>
             <TableCell align="right">Status</TableCell>
           </TableRow>
         </TableHead>
@@ -35,6 +44,15 @@ const CustomTable = () => {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
+              
+              <TableCell component="th" scope="row">
+                {row.pickup}
+              </TableCell>
+              
+              <TableCell component="th" scope="row">
+                {row.delivery}
+              </TableCell>
+
               <TableCell align="right" onClick={() => toggleStatus(row.id)} style={{ cursor: 'pointer' }}>
                 {row.status}
               </TableCell>
